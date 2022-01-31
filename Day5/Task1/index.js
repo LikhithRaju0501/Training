a5dd1ce14e5dbd5bd62e068c237208486096c4c7
Likhith Raju <likhith.raju@ibm.com>
2022-01-31 16:48:42 +0530

    second commit

---
+++ Day5/Task1/index.js
@@ -0,0 +1,13 @@
+var a = 2;
+var b = 3;
+console.log("a=", a, "b=", b);
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Is a not equal to b?:", a != b);
+console.log("a Incremented :", a++);
+console.log("a Decremented:", a--);
+console.log("a Incremented by b units:", (a += b));
+console.log("a Decremented by b units:", (a -= b));
